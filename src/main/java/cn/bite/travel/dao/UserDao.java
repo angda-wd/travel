d3a491d55f67da52ee04591be7c9b885936a9091
angda_wd <2908049163@qq.com>
2019-08-17 23:20:58 +0800

    Dao

--- src/main/java/cn/bite/travel/dao/UserDao.java
+++ src/main/java/cn/bite/travel/dao/UserDao.java
@@ -1,5 +1,20 @@
 package cn.bite.travel.dao;
 
+import cn.bite.travel.domain.User;
+
 public interface UserDao {
+
+    /**
+     * 查找信息
+     * @param username
+     * @return
+     */
+    public User findByUsername(String username);
+
+    /**
+     * 保存信息
+     * @param user
+     */
+    public void save(User user);
 
 }
--- src/main/java/cn/bite/travel/dao/impl/UserDaoImpl.java
+++ src/main/java/cn/bite/travel/dao/impl/UserDaoImpl.java
@@ -1,7 +1,34 @@
 package cn.bite.travel.dao.impl;
 
 import cn.bite.travel.dao.UserDao;
+import cn.bite.travel.domain.User;
+import cn.bite.travel.util.JDBCUtils;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDaoImpl implements UserDao {
+    private JdbcTemplate template=new JdbcTemplate(JDBCUtils.getDataSource());
+    @Override
+    public User findByUsername(String username) {
+        String sql="select * from tab_user where username=?";
 
+        User user = template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), username);
+        return user;
+    }
+
+    @Override
+    public void save(User user) {
+
+        String sql="insert into tab_User(username,password,name,birthday,sex,temephont,email) value(?,?,?,?,?,?.?)";
+
+        template.update(sql,user.getUsername(),
+                user.getPassword(),
+                user.getName(),
+                user.getBirthday(),
+                user.getSex(),
+                user.getTelephone(),
+                user.getEmail()
+                );
+    }
 }
